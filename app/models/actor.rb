c43e75614f0dd37923aef211e86ed759a19f6524
Tam Vu <vutam_a@hotmail.com>
2019-10-13 23:23:36 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,4 +6,10 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end 
   
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end 
+    
+  end 
 end
